baa57b30585dc6a143b071103df6d75746e27c54
Natalia <natalia.datascience@gmail.com>
2022-02-09 20:09:57 -0300

    Add MirageJs and Axios to mock server api

--- src/components/TransactionTable/styles.ts
+++ src/components/TransactionTable/styles.ts
@@ -18,7 +18,7 @@ export const Container = styled.main `
    td {
      padding: 1rem 2rem;
      border: 0;
-     background: var()(--shape);
+     background: var(--shape);
      color: var(--text-body);
      border-radius: 0.25rem;
 
