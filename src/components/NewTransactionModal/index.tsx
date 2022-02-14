ae79b8da4cf51cde9eae43ca5e84c579f76947aa
Natalia <natalia.datascience@gmail.com>
2022-02-14 16:40:51 -0300

    Add transaction in fake API

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,7 @@ import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
 import { Container, RadioBox, TransactionTypeContainer } from '../NewTransactionModal/styles';
 import { FormEvent, useState } from 'react';
+import { api } from '../../services/api';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -18,6 +19,12 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
   
   function handleCreateNewTransaction(event: FormEvent){
     event.preventDefault();
+
+    const data = {
+      title, value, category, type
+    };
+
+    api.post('/transactions', data)
   }
 
   return (
