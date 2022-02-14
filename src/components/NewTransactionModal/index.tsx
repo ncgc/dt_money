a80add9505868cffd8abb9531aa02ef7ce127c4e
Natalia <natalia.datascience@gmail.com>
2022-02-14 15:29:44 -0300

    Add new transaction mode style

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,9 @@
 import Modal from 'react-modal';
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import closeImg from '../../assets/close.svg'
+import { Container, RadioBox, TransactionTypeContainer } from '../NewTransactionModal/styles';
+import { useState } from 'react';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -6,12 +11,51 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps){
+  const [type, setType] = useState('deposit');
+  
   return (
     <Modal 
     isOpen={isOpen} 
-    onRequestClose={onRequestClose}>
+    onRequestClose={onRequestClose}
+    overlayClassName='react-modal-overlay'
+    className='react-modal-content'>
     
+    <button 
+      type="button" 
+      onClick={onRequestClose} 
+      className='react-modal-close'>
+
+      <img src={closeImg} alt="Fechar modal"/>
+    </button>
+
+    <Container>
     <h2> Cadastrar Transação</h2>
+
+    <input placeholder='Título'/>
+    <input type = 'number' placeholder='Valor'/>
+
+    <TransactionTypeContainer>
+      <RadioBox
+        type="button"
+        onClick={() => {setType('deposit')}}
+        isActive={type === 'deposit'} >
+
+        <img src={incomeImg} alt="Entrada"/>
+        <span>Entrada</span>
+      </RadioBox>
+
+      <RadioBox
+        type="button" 
+        onClick = {() => {setType('withdrawl')}}
+        isActive = {type === 'withdrawl'} >
+        <img src={outcomeImg} alt="Saída"/>
+        <span>Entrada</span>
+      </RadioBox>
+    </TransactionTypeContainer>
+
+    <input placeholder='Categoria'/>
+    <button type="submit">Cadastrar</button>
+    </Container>
   </Modal>
   )
 }
