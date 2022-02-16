b8a9472bdfd7a74ec41f44d98ab34357f260fb6d
Natalia <natalia.datascience@gmail.com>
2022-02-16 17:08:18 -0300

    Change createTransaction as async and clean form

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -18,7 +18,7 @@ type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
@@ -38,6 +38,6 @@ export function TransactionsProvider({children}: TransactionsProviderProps){
   </TransactionsContext.Provider>
 }
 
-function createTransaction(transaction: TransactionInput){
-  api.post('/transactions', transaction)
+async function createTransaction(transaction: TransactionInput){
+  await api.post('/transactions', transaction)
 }
